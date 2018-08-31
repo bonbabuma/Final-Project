6c3de66ccff80d198424eb4d3f63ac2ee5dff167
Jason Monette <jasonmonette44@gmail.com>
2018-08-31 17:23:25 -0600

    almost done main pages UI layouts

--- FinalProject/index.js
+++ FinalProject/index.js
@@ -5,32 +5,40 @@ const url = 'mongodb://localhost:27017'
 const port = 3000;
 
 server.set('view engine', 'pug');
-server.use(express.static('public'));
+server.use(express.static(__dirname+'/public'));
 
 server.get('/', (req, res) => {
     res.render('homepage', {})
 });
-    server.use(express.static(__dirname+'/public'));
+    
 server.get('/company/createAccount', (req, res) => {
     res.render('cmpCreate', {})
 });
-
 server.get('/user/createAccount', (req, res) => {
     res.render('userCreate', {})
 });
-    server.use(express.static(__dirname+'/public'));
 server.get('/company', (req, res) => {
-    res.render('company.pug', {})
+    //create account
+    let companyName = req.query.companyName
+
+    res.render('companyAccount', {companyName})
 }); 
 server.get('/user', (req, res) => {
+    //create account
+    let companyName = req.query.companyName;
+    let username = req.query.username;
+    let password = req.query.password;
+
+  
+
     MongoClient.connect(url, function(err, client) {
     console.log('connected to the mongoDB server');
 
     const db = client.db('FinalProject');
     const collection = db.collection('userAccounts');
-    collection.find({}).toArray((err, documents) => {
-        res.render('userAccount', {documents: documents});
-        client.close(); 
+    collection.find({}).toArray((err, document) => {
+        res.render('userAccount', {username, companyName, document});
+        client.close();
     });
 });
 })
