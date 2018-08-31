4337d1b6c0574b808554ad4aa78d5acf8181abfb
Jason Monette <jasonmonette44@gmail.com>
2018-08-30 23:41:46 -0600

    worked a lot on FinalProject

--- FinalProject/index.js
+++ FinalProject/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const server = express();
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb://localhost:27017'
 const port = 3000;
 
 server.set('view engine', 'pug');
@@ -9,13 +11,30 @@ server.get('/', (req, res) => {
     res.render('homepage', {})
 });
     server.use(express.static(__dirname+'/public'));
-server.get('/schedule', (req, res) => {
-    res.render('schedule', {})
+server.get('/company/createAccount', (req, res) => {
+    res.render('cmpCreate', {})
+});
+
+server.get('/user/createAccount', (req, res) => {
+    res.render('userCreate', {})
 });
     server.use(express.static(__dirname+'/public'));
 server.get('/company', (req, res) => {
     res.render('company.pug', {})
 }); 
+server.get('/user', (req, res) => {
+    MongoClient.connect(url, function(err, client) {
+    console.log('connected to the mongoDB server');
+
+    const db = client.db('FinalProject');
+    const collection = db.collection('userAccounts');
+    collection.find({}).toArray((err, documents) => {
+        res.render('userAccount', {documents: documents});
+        client.close(); 
+    });
+});
+})
+
 
 server.listen(port, (err) => {
     if (err) {
